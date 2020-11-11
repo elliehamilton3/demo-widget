98a986fc582fe6a978387fc91c7169bd6fb737e0
Ellie Hamilton <eleanorhamilton77@gmail.com>
2020-11-11 15:46:24 +1300

    Add basic styles to widget

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
-import Widget from '../components/widget'
+import Widget from "../components/widget";
+// import WidgetMaterial from '../components/widget.material';
+
 export default function Home() {
   return (
     <>
-    <div>Home</div>
-    <Widget/>
+      <div style={{ padding: "20px", maxWidth: "500px" }}>
+        <Widget
+          title={"Financial and Monetary Systems"}
+          link={"https://intelligence.weforum.org/topics/a1Gb0000000LHOUEA4?tab=publications"}
+          imageLink={
+            "https://assets.weforum.org/topic/transformation_map_image/kSAv4-JRsZFgQTylLjadZR9D-Zag1ieUZA2IiQwd_zQ.png"
+          }
+        />
+        {/* Dynamic: image, title, link */}
+        {/* Configurable: Colours, fonts, layouts? */}
+        {/* Tidy: Padding, layout, responsive, image loading perf improvements */}
+      </div>
+      {/* <WidgetMaterial />  */}
     </>
   );
 }
